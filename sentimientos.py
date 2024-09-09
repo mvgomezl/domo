32dd94df178b6ec944a94c4d7e583569076f5405
mvgomezl <vanessbunny@gmail.com>
2024-09-08 20:21:45 -0500

    Update sentimientos.py
    test
    
    Co-Authored-By: Oscar Correcha <48962667+oecorrechag@users.noreply.github.com>

--- sentimientos.py
+++ sentimientos.py
@@ -1,6 +1,5 @@
 print('''
 
 Soujiro Seta is a pivotal character in Rurouni Kenshin. A young prodigy with immense swordsmanship talent, he's drawn into the conflict between Kenshin and Shishio Makoto. Soujiro's unwavering loyalty to Shishio and his desire for strong opponents make him a formidable adversary. His tragic past and eventual realization of his own humanity lead to a poignant and complex character arc.
-doot
 
 ''')

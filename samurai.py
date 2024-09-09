3003324af0383ee9a5e9638e32d2ce8adf196fe4
oecorrechag <75684931+mvgomezl@users.noreply.github.com>
2024-09-08 19:36:13 -0500

    que anime

---
+++ samurai.py
@@ -0,0 +1,8 @@
+print('''
+
+
+The samurai were a class of warriors in feudal Japan. They were known for their loyalty to their daimyo (feudal lord) and their strict code of conduct, bushido. Bushido emphasized virtues such as courage, honor, and loyalty.
+
+Samurai society flourished during the Edo period (1603-1868), a time of relative peace and stability in Japan. However, with the Meiji Restoration in 1868, the samurai class was abolished and Japan began to modernize.
+
+''')
